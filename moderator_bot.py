05c6d2aaa5735d5ba32c6b9c2d31f826e68a5887
drakoneans <86190782+drakoneans@users.noreply.github.com>
2021-06-26 22:07:47 +0600

    Update moderator_bot.py

--- moderator_bot.py
+++ moderator_bot.py
@@ -1,3 +1,4 @@
+
 import telebot
 
 with open('MODER_TOKEN.txt') as t:
@@ -9,6 +10,22 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'hi')
+    if message.from_user.is_bot:
+        return
+    bot.send_message(message.user.id, 'hi')
+
+@bot.message_handler(content_types=['text'])
+def text(message):
+    bot_name = '@twibuysellbot'
+    moderators = ['@swen13066', 'drakoneans']
+    if message.from_user.username == bot_name:
+        bot.forward_message(moderators[0], bot_name, message.message_id)
+    if message.from_user.username in moderators:
+        msg_id = message.reply_to_mesage.forward_from_message_id
+        bot.send_message(bot_name, f'{msg_id}\n{1 if message.text == "1" else 0 }')
+    else:
+        bot.send_message(message.chat.id, 'ТЫ НЕ РОВЕРЗЕТ')
+
+
 
 bot.polling()
